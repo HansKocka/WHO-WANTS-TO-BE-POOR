9acad70361b2879cf9ca7698170774e7974875b1
HansKočka <dudlik700@gmail.com>
2026-06-05 22:36:52 +0200

    icc
    stattt

--- server/server/settings.py
+++ server/server/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+STATIC_URL = "/static/"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
