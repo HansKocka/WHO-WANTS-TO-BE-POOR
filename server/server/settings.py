674c5d96ba48cdb1c4508f743a7a8487820da2f8
HansKočka <dudlik700@gmail.com>
2026-06-19 16:48:46 +0200

    ggg
    ggg

--- server/server/settings.py
+++ server/server/settings.py
@@ -121,8 +121,9 @@ STATIC_URL = "/static/"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.seznam.cz"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
